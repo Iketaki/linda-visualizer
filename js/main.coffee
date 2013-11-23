ec0f3bb8402bb385df9f305349f57190ae8befa8
Shunta Iketaki <s.iketaki@gmail.com>
2013-11-23 10:14:52 +0900

    support watching multiple tuplespaces

--- js/main.coffee
+++ js/main.coffee
@@ -27,7 +27,8 @@ Array::tuple_str = ->
 $ ->
   io = new RocketIO().connect("http://linda.masuilab.org")
   linda = new Linda(io)
-  delta = new linda.TupleSpace("delta")
+  tuple_spaces = ["delta", "iota", "enoshima", "orf"]
+  #delta = new linda.TupleSpace("delta")
 
   # init
   width = $("#visual").height()
@@ -46,133 +47,143 @@ $ ->
     $("#status").text "#{io.type} connect"
 
     data = {
-      key: "delta",
+      key: "linda.masuilab.org",
       children: []
     }
 
-    # tuple
-    delta.watch [], (tuple) =>
-      $("#list").prepend $("<p>").text("[#{tuple.join(", ")}]")
+    watch_ts = (ts) ->
+      data.children.push
+        key: ts.name
+        children: []
 
-      # update data
-      # 第一階層 sensor
-      # [sensor, light, 3]
-      root = data
+      child_index = data.children.length - 1
 
-      i = 0; while i < root.children.length
-        if root.children[i].key == tuple[0]
-          break
-        i += 1
+      ts.watch [], (tuple) =>
+        $("#list").prepend $("<p>").text("[#{tuple.join(", ")}]")
 
-      # なかったら
-      if i == root.children.length
-        h = {
-            key: tuple[0]
-            children: []
-        }
-        root.children.push h
+        # update data
+        # 第一階層 sensor
+        # [sensor, light, 3]
+        root = data.children[child_index]
 
-      root = root.children[i]
+        i = 0; while i < root.children.length
+          if root.children[i].key == tuple[0]
+            break
+          i += 1
 
-      updated_key = tuple.make_key(2)
-      i = 0; while i < root.children.length
-        if root.children[i].key == updated_key
-          break
-        i += 1
+        # なかったら
+        if i == root.children.length
+          h = {
+              key: tuple[0]
+              children: []
+          }
+          root.children.push h
 
-      if i == root.children.length
-        h = {
-            key: updated_key
-            value: 1
-            tuple: tuple
-        }
-        root.children.push h
-      else
-        root.children[i].value += 1
-        root.children[i].tuple = tuple
+        root = root.children[i]
 
-      # packing algorithhm
-      flattened = flatten(data)
-      nodes = pack.nodes(flattened)
+        updated_key = tuple.make_key(2)
+        i = 0; while i < root.children.length
+          if root.children[i].key == updated_key
+            break
+          i += 1
 
-      # for debug
-      window.data = data
-      window.flattened = flattened
-      window.nodes = nodes
+        if i == root.children.length
+          h = {
+              key: updated_key
+              value: 1
+              tuple: tuple
+          }
+          root.children.push h
+        else
+          root.children[i].value += 1
+          root.children[i].tuple = tuple
 
-      # draws
-      elems = svg.selectAll(".node")
-        .data(nodes)
+        # packing algorithhm
+        flattened = flatten(data)
+        nodes = pack.nodes(flattened)
 
-      window.elems = elems
+        # for debug
+        window.data = data
+        window.flattened = flattened
+        window.nodes = nodes
 
-      # Append
-      appended = elems.enter().append("g")
-        .attr("class", "node")
-        .attr "transform", (d) ->
-          "translate(#{d.x}, #{d.y})"
+        # draws
+        elems = svg.selectAll(".node")
+          .data(nodes)
 
-      circle = appended.append("circle")
-        .attr
-          "fill-opacity": 0.3
-          "stroke-opacity": 0.3
-          "stroke-width": 2
-          "stroke": (d, i) =>
-            "#ffffff"
-          "fill": (d, i) =>
+        window.elems = elems
+
+        # Append
+        appended = elems.enter().append("g")
+          .attr("class", "node")
+          .attr "transform", (d) ->
+            "translate(#{d.x}, #{d.y})"
+
+        circle = appended.append("circle")
+          .attr
+            "fill-opacity": 0.3
+            "stroke-opacity": 0.3
+            "stroke-width": 2
+            "stroke": (d, i) =>
+              "#ffffff"
+            "fill": (d, i) =>
+              color(i)
+          .attr("r", 0)
+          .transition()
+          .duration(700)
+          .ease("bounce")
+          .attr "r", (d) ->
+            d.r
+
+        text_attr =
+          "fill": "white"
+          "text-anchor": "middle"
+          "alignment-baseline": "middle"
+        text_style =
+          "text-shadow": "#000000 0 -2px 0"
+
+        appended.append("text")
+          .attr("class", "tuple")
+          .text (d) -> #[TODO] work?
+            return "" unless d.tuple
+            "[#{d.tuple.join(", ")}]"
+          .attr(text_attr)
+          .style(text_style)
+
+        appended.append("text")
+          .attr("class", "value")
+          .text (d) ->
+            ""
+          .attr("dy", "2em")
+          .attr(text_attr)
+          .style(text_style)
+
+        # Update
+        elems
+          .transition()
+          .duration(700)
+          .attr "transform", (d) ->
+            "translate(#{d.x}, #{d.y})"
+
+        elems.select("circle")
+          .attr "fill", (d, i) ->
+            if d.tuple && d.tuple.make_key(2) == updated_key then "white" else color(i)
+          .attr "fill-opacity", (d, i) ->
+            if d.tuple && d.tuple.make_key(2) == updated_key then 0.8 else 0.3
+          .transition()
+          .duration(300)
+          .attr "fill", (d, i) =>
             color(i)
-        .attr("r", 0)
-        .transition()
-        .duration(700)
-        .ease("bounce")
-        .attr "r", (d) ->
-          d.r
+          .attr("fill-opacity", 0.3)
+          .attr "r", (d) ->
+            d.r
 
-      text_attr =
-        "fill": "white"
-        "text-anchor": "middle"
-        "alignment-baseline": "middle"
-      text_style =
-        "text-shadow": "#000000 0 -2px 0"
+        elems.select(".tuple").each (d) ->
+          this.textContent = "[#{d.tuple.slice(0, 2).join(', ')}]" if d.tuple
 
-      appended.append("text")
-        .attr("class", "tuple")
-        .text (d) -> #[TODO] work?
-          return "" unless d.tuple
-          "[#{d.tuple.join(", ")}]"
-        .attr(text_attr)
-        .style(text_style)
+        elems.select(".value").each (d) ->
+          this.textContent = d.value if d.tuple
 
-      appended.append("text")
-        .attr("class", "value")
-        .text (d) ->
-          ""
-        .attr("dy", "2em")
-        .attr(text_attr)
-        .style(text_style)
-
-      # Update
-      elems
-        .transition()
-        .duration(700)
-        .attr "transform", (d) ->
-          "translate(#{d.x}, #{d.y})"
-
-      elems.select("circle")
-        .attr "fill", (d, i) ->
-          if d.tuple && d.tuple.make_key(2) == updated_key then "white" else color(i)
-        .attr "fill-opacity", (d, i) ->
-          if d.tuple && d.tuple.make_key(2) == updated_key then 0.8 else 0.3
-        .transition()
-        .duration(300)
-        .attr "fill", (d, i) =>
-          color(i)
-        .attr("fill-opacity", 0.3)
-        .attr "r", (d) ->
-          d.r
-
-      elems.select(".tuple").each (d) ->
-        this.textContent = "[#{d.tuple.slice(0, 2).join(', ')}]" if d.tuple
-
-      elems.select(".value").each (d) ->
-        this.textContent = d.value if d.tuple
+    # tuple
+    for ts in tuple_spaces
+      watch_ts(new linda.TupleSpace(ts))
--- js/main.js
+++ js/main.js
@@ -34,11 +34,11 @@
   };
 
   $(function() {
-    var color, data, delta, height, io, linda, pack, svg, width,
+    var color, data, height, io, linda, pack, svg, tuple_spaces, width,
       _this = this;
     io = new RocketIO().connect("http://linda.masuilab.org");
     linda = new Linda(io);
-    delta = new linda.TupleSpace("delta");
+    tuple_spaces = ["delta", "iota", "enoshima", "orf"];
     width = $("#visual").height();
     height = $("#visual").height();
     svg = d3.select("#visual").append("svg").attr("width", width).attr("height", height);
@@ -46,120 +46,136 @@
     data = [];
     color = d3.scale.category20();
     return io.on("connect", function() {
+      var ts, watch_ts, _i, _len, _results;
       $("#status").text("" + io.type + " connect");
       data = {
-        key: "delta",
+        key: "linda.masuilab.org",
         children: []
       };
-      return delta.watch([], function(tuple) {
-        var appended, circle, elems, flattened, h, i, nodes, root, text_attr, text_style, updated_key;
-        $("#list").prepend($("<p>").text("[" + (tuple.join(", ")) + "]"));
-        root = data;
-        i = 0;
-        while (i < root.children.length) {
-          if (root.children[i].key === tuple[0]) {
-            break;
-          }
-          i += 1;
-        }
-        if (i === root.children.length) {
-          h = {
-            key: tuple[0],
-            children: []
-          };
-          root.children.push(h);
-        }
-        root = root.children[i];
-        updated_key = tuple.make_key(2);
-        i = 0;
-        while (i < root.children.length) {
-          if (root.children[i].key === updated_key) {
-            break;
-          }
-          i += 1;
-        }
-        if (i === root.children.length) {
-          h = {
-            key: updated_key,
-            value: 1,
-            tuple: tuple
-          };
-          root.children.push(h);
-        } else {
-          root.children[i].value += 1;
-          root.children[i].tuple = tuple;
-        }
-        flattened = flatten(data);
-        nodes = pack.nodes(flattened);
-        window.data = data;
-        window.flattened = flattened;
-        window.nodes = nodes;
-        elems = svg.selectAll(".node").data(nodes);
-        window.elems = elems;
-        appended = elems.enter().append("g").attr("class", "node").attr("transform", function(d) {
-          return "translate(" + d.x + ", " + d.y + ")";
+      watch_ts = function(ts) {
+        var child_index,
+          _this = this;
+        data.children.push({
+          key: ts.name,
+          children: []
         });
-        circle = appended.append("circle").attr({
-          "fill-opacity": 0.3,
-          "stroke-opacity": 0.3,
-          "stroke-width": 2,
-          "stroke": function(d, i) {
-            return "#ffffff";
-          },
-          "fill": function(d, i) {
-            return color(i);
+        child_index = data.children.length - 1;
+        return ts.watch([], function(tuple) {
+          var appended, circle, elems, flattened, h, i, nodes, root, text_attr, text_style, updated_key;
+          $("#list").prepend($("<p>").text("[" + (tuple.join(", ")) + "]"));
+          root = data.children[child_index];
+          i = 0;
+          while (i < root.children.length) {
+            if (root.children[i].key === tuple[0]) {
+              break;
+            }
+            i += 1;
           }
-        }).attr("r", 0).transition().duration(700).ease("bounce").attr("r", function(d) {
-          return d.r;
-        });
-        text_attr = {
-          "fill": "white",
-          "text-anchor": "middle",
-          "alignment-baseline": "middle"
-        };
-        text_style = {
-          "text-shadow": "#000000 0 -2px 0"
-        };
-        appended.append("text").attr("class", "tuple").text(function(d) {
-          if (!d.tuple) {
+          if (i === root.children.length) {
+            h = {
+              key: tuple[0],
+              children: []
+            };
+            root.children.push(h);
+          }
+          root = root.children[i];
+          updated_key = tuple.make_key(2);
+          i = 0;
+          while (i < root.children.length) {
+            if (root.children[i].key === updated_key) {
+              break;
+            }
+            i += 1;
+          }
+          if (i === root.children.length) {
+            h = {
+              key: updated_key,
+              value: 1,
+              tuple: tuple
+            };
+            root.children.push(h);
+          } else {
+            root.children[i].value += 1;
+            root.children[i].tuple = tuple;
+          }
+          flattened = flatten(data);
+          nodes = pack.nodes(flattened);
+          window.data = data;
+          window.flattened = flattened;
+          window.nodes = nodes;
+          elems = svg.selectAll(".node").data(nodes);
+          window.elems = elems;
+          appended = elems.enter().append("g").attr("class", "node").attr("transform", function(d) {
+            return "translate(" + d.x + ", " + d.y + ")";
+          });
+          circle = appended.append("circle").attr({
+            "fill-opacity": 0.3,
+            "stroke-opacity": 0.3,
+            "stroke-width": 2,
+            "stroke": function(d, i) {
+              return "#ffffff";
+            },
+            "fill": function(d, i) {
+              return color(i);
+            }
+          }).attr("r", 0).transition().duration(700).ease("bounce").attr("r", function(d) {
+            return d.r;
+          });
+          text_attr = {
+            "fill": "white",
+            "text-anchor": "middle",
+            "alignment-baseline": "middle"
+          };
+          text_style = {
+            "text-shadow": "#000000 0 -2px 0"
+          };
+          appended.append("text").attr("class", "tuple").text(function(d) {
+            if (!d.tuple) {
+              return "";
+            }
+            return "[" + (d.tuple.join(", ")) + "]";
+          }).attr(text_attr).style(text_style);
+          appended.append("text").attr("class", "value").text(function(d) {
             return "";
-          }
-          return "[" + (d.tuple.join(", ")) + "]";
-        }).attr(text_attr).style(text_style);
-        appended.append("text").attr("class", "value").text(function(d) {
-          return "";
-        }).attr("dy", "2em").attr(text_attr).style(text_style);
-        elems.transition().duration(700).attr("transform", function(d) {
-          return "translate(" + d.x + ", " + d.y + ")";
-        });
-        elems.select("circle").attr("fill", function(d, i) {
-          if (d.tuple && d.tuple.make_key(2) === updated_key) {
-            return "white";
-          } else {
+          }).attr("dy", "2em").attr(text_attr).style(text_style);
+          elems.transition().duration(700).attr("transform", function(d) {
+            return "translate(" + d.x + ", " + d.y + ")";
+          });
+          elems.select("circle").attr("fill", function(d, i) {
+            if (d.tuple && d.tuple.make_key(2) === updated_key) {
+              return "white";
+            } else {
+              return color(i);
+            }
+          }).attr("fill-opacity", function(d, i) {
+            if (d.tuple && d.tuple.make_key(2) === updated_key) {
+              return 0.8;
+            } else {
+              return 0.3;
+            }
+          }).transition().duration(300).attr("fill", function(d, i) {
             return color(i);
-          }
-        }).attr("fill-opacity", function(d, i) {
-          if (d.tuple && d.tuple.make_key(2) === updated_key) {
-            return 0.8;
-          } else {
-            return 0.3;
-          }
-        }).transition().duration(300).attr("fill", function(d, i) {
-          return color(i);
-        }).attr("fill-opacity", 0.3).attr("r", function(d) {
-          return d.r;
+          }).attr("fill-opacity", 0.3).attr("r", function(d) {
+            return d.r;
+          });
+          elems.select(".tuple").each(function(d) {
+            if (d.tuple) {
+              return this.textContent = "[" + (d.tuple.slice(0, 2).join(', ')) + "]";
+            }
+          });
+          return elems.select(".value").each(function(d) {
+            if (d.tuple) {
+              return this.textContent = d.value;
+            }
+          });
         });
-        elems.select(".tuple").each(function(d) {
-          if (d.tuple) {
-            return this.textContent = "[" + (d.tuple.slice(0, 2).join(', ')) + "]";
-          }
-        });
-        return elems.select(".value").each(function(d) {
-          if (d.tuple) {
-            return this.textContent = d.value;
-          }
-        });
-      });
+      };
+      _results = [];
+      for (_i = 0, _len = tuple_spaces.length; _i < _len; _i++) {
+        ts = tuple_spaces[_i];
+        _results.push(watch_ts(new linda.TupleSpace(ts)));
+      }
+      return _results;
     });
   });
 
